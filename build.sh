d865a75db9ee9f9012be82da568793b515653956
Alexander Renz <renz@uuxo.net>
2024-10-14 11:24:01 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 ### get VERSIONSTRING
 VERSIONSTRING="$(git describe --tags --exact-match || git rev-parse --short HEAD)"
 
-echo "Building version ${VERSIONSTRING} of Prosody-Filer ..."
+echo "Building version ${VERSIONSTRING} of HMAC File Server ..."
 
 ### Compile and link statically
 CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-extldflags '-static' -w -s -X main.versionString=${VERSIONSTRING}" hmac-file-server.go
-
